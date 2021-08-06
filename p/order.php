b91cd50b8042c9dc772992b5fa972c03161c3273
albin <albin@senioradministrator.com>
2021-08-06 13:24:45 -0400

    q

--- p/order.php
+++ p/order.php
@@ -33,17 +33,97 @@
 
                   <?php
                   //fetch order info
+                  $fetch_order = "SELECT o_status,o_PP,o_date FROM t_orders WHERE `o_id`='$orderid' AND `o_owner_cid`='$customerSessionId';";
+                  $fetch_order = $conn->query($fetch_order);
+                  if ($fetch_order->num_rows > 0)
+                  {
+                    while($row = $fetch_order->fetch_assoc())
+                    {
+                        $oo_post_price = $row['o_PP'];
+                        $oo_date = $row['o_date'];
+                      ?>
+                            <h3>شماره سفارش : <span id="order-id"><?php echo $orderid;?></span></h3>
+                            <?php
+                            switch ($row['o_status'])
+                            {
+                              case '0':
+                              {
+                                  $ostatus_text = "لغو شده";
+                              }break;
+
+                              case '1':
+                              {
+                                  $ostatus_text = "تحویل شده";
+                              }break;
+
+                              case '2':
+                              {
+                                  $ostatus_text = "در انتظار پرداخت";
+                              }break;
+
+                              default:
+                              {
+                                  $ostatus_text = "در حال پردازش";
+                              }break;
+                            }
+                            ?>
+
+                            <h3>وضعیت : <span id="order-status"><?php echo $ostatus_text;?></span></h3>
+                          </div>
+                          <div id="left-order-detail-sub">
+
+
+
+                       <?php
+                    }
+                  }
+                  else
+                    show_result("error","error while loading customer data","","","Lc Melody","current"); //mode , text , button lable , button target ,title , window (current)
+
+
+
+
+                    //for count total price order
+                    $oo_amounts = array();
+                    $oo_price = array();
+                    $oo_discount = array();
+
+                    //fetch order items
+                    $fetch_orderitems = "SELECT oi_p_id,oi_amount,oi_price,oi_discount FROM t_orders_items WHERE `oi_o_id`='$orderid';";
+                    $result_fetch_orderitems = $conn->query($fetch_orderitems);
+                    if ($result_fetch_orderitems->num_rows > 0)
+                    {
+                      while($row = $result_fetch_orderitems->fetch_assoc())
+                      {
+
+                             fetch_product_information($row['oi_p_id'],$row['oi_amount'],$row['oi_price'],$row['oi_discount']);
+                             //for total cost
+                             array_push($oo_amounts,str_replace(",","",$row['oi_amount']));
+                             array_push($oo_price,str_replace(",","",$row['oi_price']));
+                             array_push($oo_discount,str_replace(",","",$row['oi_discount']));
+                      }
+                    }
+                    else
+                      show_result("error","error while loading order items","","","Lc Melody","current"); //mode , text , button lable , button target ,title , window (current)
+
+                    //count total cost
+                    $total_order_cost=0;
+                    $total_order_cost=array_map(function ($oo_price,$oo_discount,$oo_amounts)------------------------
+                    {
+                        $total = $oo_price - $oo_discount;
+                        $GLOBALS['$total_order_cost'] += $total*$oo_amounts;
+                    }, $oo_price,$oo_discount,$oo_amounts);
+                    $cost_tol = $GLOBALS['$total_order_cost'] + str_replace(",","",$oo_post_price);
 
 
 
                   ?>
-                  <h3>شماره سفارش : <span id="order-id">۴۵۶۳</span></h3>
-                  <h3>وضعیت : <span id="order-status">در انتظار پرداخت</span></h3>
-                </div>
-                <div id="left-order-detail-sub">
-                  <h3>مبلغ کل : <span id="order-price">۱۲۳,۱۲۳,۱۲۳</span></h3>
-                  <h3>هزینه پست : <span id="order-post-price">۱۲۳,۱۲۳,۱۲۳</span></h3>
-                  <h3>تاریخ ثبت سفارش : <span id="order-date">۱۲/۱۲/۱۳۱۲</span></h3>
+
+
+                                              <h3>مبلغ کل : <span id="order-price"><?php echo $cost_tol;?></span></h3>
+                                              <h3>هزینه پست : <span id="order-post-price"><?php echo $oo_post_price;?></span></h3>
+                                              <h3>تاریخ ثبت سفارش : <span id="order-date"><?php echo $oo_date;?></span></h3>
+
                 </div>
           </div>
 
@@ -56,21 +136,7 @@
 
 
             <?php
-            //fetch order items
-            $fetch_orderitems = "SELECT oi_p_id,oi_amount,oi_price,oi_discount FROM t_orders_items WHERE `oi_o_id`='$orderid';";
-            $result_fetch_orderitems = $conn->query($fetch_orderitems);
-            if ($result_fetch_orderitems->num_rows > 0)
-            {
-              while($row = $result_fetch_orderitems->fetch_assoc())
-              {
-                     fetch_product_information($row['oi_p_id'],$row['oi_amount'],$row['oi_price'],$row['oi_discount']);
-              }
-            }
-            else
-              show_result("error","error while loading customer data","","","Lc Melody","current"); //mode , text , button lable , button target ,title , window (current)
-
-
-
+              //set order items
               function fetch_product_information($productId,$amount,$price,$discount)
               {
                   //fetch product information requirement
--- p/product.php
+++ p/product.php
@@ -1,6 +1,44 @@
     <?php
-        require ('../includes/security.php');
+        // require ('../includes/security.php');
         include ('../includes/header.php');
+
+
+
+        $servername = "localhost";
+        $username = "me";
+        $password = "amx";
+        $dbname = "lc3";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error)
+        {
+          show_result("error","خطا در ارتباط با پایگاه داده","","","Database Error","current");
+        }
+
+        $proId = $_GET['id'];
+        if($proId=="")
+        {
+          show_result("error","لطفا مقادیر معتبر را وارد کنید","","","Lc Melody","current"); //mode , text , button lable , button target ,title , window (current)
+        }
+
+        //fetch info
+        $fetch_product_info = "SELECT p_title,p_amount,p_discount,p_price,p_summary_desc,p_description FROM t_product WHERE `p_id`='$proId';";
+        $result_pro_info = $conn->query($fetch_product_info);
+        if ($result_pro_info->num_rows > 0)
+        {
+          while($row = $result_pro_info->fetch_assoc())
+          {
+                $pTitle = $row['p_title'];
+                $pAmount = $row['p_amount'];
+                $pPrice = $row['p_price'];
+                $pTechinical = $row['p_summary_desc'];
+                $pDescrip = $row['p_description'];
+                $pDiscount = $row['p_discount'];
+
+          }
+        }
+
+        //fetch comments
+
     ?>
         <title>میکروفن داینامیک شور مدل SM</title>
         <link rel="stylesheet" href="../assets/css/product.css">
@@ -23,34 +61,73 @@
               <img id="main-picture-product" src="../assets/img/p-images/p100.jpg" alt="some"/>
             <div id="small-preview-pictures-of-product-box">
               <br/> <br/> <br/>
-              <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/>
-              <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/>
-              <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/>
-              <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/>
+              <?php
+
+                check_product_image_exists($proId,'a');
+                check_product_image_exists($proId,'b');
+                check_product_image_exists($proId,'c');
+                check_product_image_exists($proId,'d');
+
+                function check_product_image_exists($id,$cha)
+                {
+                  if(file_exists("../assets/img/p-images/".$id."-a.jpg"))
+                  {
+                    ?>
+                      <img class="small-picture-preview def-border" src="../assets/img/p-images/<?php echo $id."-".$cha;?>.jpg" alt="product-<?php echo $id;?>-image"/>
+                    <?php
+                  }
+                  else
+                  {
+                    ?>
+                      <img class="small-picture-preview def-border" src="../assets/img/p-images/default.jpg" alt="default-image"/>
+                    <?php
+                  }
+                }
+
+
+
+
+              ?>
+              <!-- <img class="small-picture-preview def-border" src="" alt="some"/> -->
+              <!-- <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/> -->
+              <!-- <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/> -->
+              <!-- <img class="small-picture-preview def-border" src="../assets/img/p-images/p100.jpg" alt="some"/> -->
             </div>
           </center>
         </div>
       <div id="product-details-box" class="def-border">
             <div id="section-a-product-details">
                 <div id="inner-section-a-product-details">
-                  <h1 id="product-title" class="set-two-font">میکروفن داینامیک شور مدل Shure SMS8 </h1>
-                  <h3 class="set-the-font">گارانتی اصالت و سلامت فیزیکی کالا گارانتی اصالت و سلامت فیزیکی کالا</h3>
-                  <h3 class="set-the-font">پیشنهاد شده توسط<span> ۹۹۹ </span>خریدار </h3>
-                  <h2 class="set-the-font"><span>۹</span> عدد باقی مانده</h2>
+                  <h1 id="product-title" class="set-two-font"><?php echo $pTitle;?></h1>
+                  <h3 class="set-the-font">گارانتی اصالت و سلامت فیزیکی</h3>
+                  <!-- <h3 class="set-the-font">پیشنهاد شده توسط<span> ۹۹۹ </span>خریدار </h3> -->
+                  <?php
+                  if($pAmount<=9)
+                  {
+                    ?>
+                    <h2 class="set-the-font"><span><?php echo $pAmount;?></span> عدد باقی مانده</h2>
+                    <?php
+                  }
+                  ?>
                   <center>
                     <div id="discount-parent">
+                      <?php
+                          //count pecent discount
+                          $count_percent = $pDiscount/$pPrice;
+                          $percent_friendly = number_format( $count_percent * 100, 2 );
+                      ?>
                       <div id="discount-box-sub" class="def-border">
-                        <h3 id="discount-number" class="set-the-font unselectable"><center>۹۸<span id="percentage-symbol">٪</span></center></h3>
+                        <h3 id="discount-number" class="set-the-font unselectable"><center><?php echo $percent_friendly;?><span id="percentage-symbol">٪</span></center></h3>
                       </div>
-                      <h2 id="discounted-price" class="set-the-font"><del>۳۳,۳۳۱,۴۰۰</del> <span class="fontsize18">تومان</span></h2>
+                      <h2 id="discounted-price" class="set-the-font"><del><?php echo $pPrice;?></del> <span class="fontsize18">تومان</span></h2>
                     </div>
                   </center>
-                  <h1 class="set-the-font"><span>۳۳,۳۳۱,۴۰۰</span> <span class="fontsize18">تومان</span></h1>
+                  <h1 class="set-the-font"><span><?php echo $pDiscount;?></span> <span class="fontsize18">تومان</span></h1>
                 </div>
             </div>
             <div id="section-b-product-details">
                 <center>
-                  <div id="base-button-add-to-my-cartlist" class="cursor-pointer unselectable">
+                  <div id="base-button-add-to-my-cartlist" class="cursor-pointer unselectable" onclick="--------------------------------------------">
                     <h2 id="button-add-to-my-cartlist" class="set-the-font">افزودن به سبد خرید</h2>
                   </div>
                 </center>
@@ -63,15 +140,7 @@
     <div class="base-product-sections def-border">
       <div class="product-introduction-box">
         <h1 class="set-the-font">معرفی محصول</h1>
-        <h2 id="product-intorduction-text" class="set-two-font">
-          یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است          لنوخلنخقهحثناخحقثناحخقیکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین grehreh
-          میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است
-          <br/><br/>
-          <i>تهصشلشصلصشاشصافتااتغ اتفغ تفتفغ تهخحفهتخحفغ تنهتن خف تخهفغهخت فهخغتنهتن خفغنت هتن خفنههنتخنفخهنخفتنختنخفغهتفتفغتتفغتغع</i>
-        یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است
-        لنوخلنخقهحثناخحقثناحخقیکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است
-        لنوخلنخقهحثناخحقثناحخقیکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است یکی از قدرتند ترین و محکم ترین میکروفن های از نظر فیزیکی است
-        </h2>
+        <h2 id="product-intorduction-text" class="set-two-font"><?php echo $pDescrip;?></h2>
         <br/>
       </div>
     </div>
@@ -83,21 +152,28 @@
           <h1 class="set-the-font">مشخصات فنی محصول</h1>
           <div id="base-technical-boxes">
               <div class="tech-info-box def-border">
-                  <p class="set-two-font tech-title">امپدانس خروجی</p>
-                  <p class="set-two-font tech-sub">150 ohmsساده </p>
-              </div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
-              <div class="tech-info-box def-border"><p class="set-two-font tech-title">امپدانس خروجی</p><p class="set-two-font tech-sub">150 ohmsساده </p></div>
 
+                <?php
+                  //regex techincal description.
+                  $turn = substr_count($pTechinical,";");
+                  for($i=1;$i<=$turn;$i++)
+                  {
+                    //save rule is like 'title:attribute;' dividers are : and ;
+                  	  $first_cut = strpos($pTechinical,";");
+                  	  $bee = substr($pTechinical,0,$first_cut+1);
+                      $pTechinical=str_replace($bee,"",$pTechinical);
+                      $second_cut = strpos($bee,":");
+                  	  $title = substr($bee,0,$second_cut);
+                      $value = str_replace($title,"",$bee);
+                      $value = str_replace([':',';'],"",$value);
+                      ?>
+                        <p class="set-two-font tech-title"><?php echo $title;?></p>
+                        <p class="set-two-font tech-sub"><?php echo $value;?></p>
+                      <?php
+                  }
+
+                ?>
+              </div>
           </div>
           <br/>
         </div>
